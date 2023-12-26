98b1f6ebc41e22ed3d2c02329d08b375ca8de869
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-12-26 19:50:31 +0300

    Fixed on edit image error

--- lib/ngepa_web/live/product_image_live/form_component.ex
+++ lib/ngepa_web/live/product_image_live/form_component.ex
@@ -26,6 +26,61 @@ defmodule NgepaWeb.ProductImageLive.FormComponent do
   end
 
   def handle_event("save", %{"product_image" => product_image_params}, socket) do
+    save_product_image(socket, socket.assigns.action, product_image_params)
+  end
+
+  defp save_product_image(socket, :edit_product_image, product_image_params) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+        dest = Path.join([:code.priv_dir(:ngepa), "static", "uploads", Path.basename(path)])
+
+        # The `static/uploads` directory must exist for `File.cp!/2`
+        # and MyAppWeb.static_paths/0 should contain uploads to work,.
+        File.cp!(path, dest)
+        {:ok, "/uploads/" <> Path.basename(dest)}
+      end)
+
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+
+    case List.first(uploaded_files) do
+      nil ->
+        new_product_image_params =
+          Map.put(product_image_params, "image_url", socket.assigns.product_image.image_url)
+
+        update_product_image(socket, new_product_image_params)
+
+      _ ->
+        full_url = "priv/static" <> List.first(uploaded_files)
+
+        {:ok, data} =
+          Cloudex.upload(full_url)
+
+        new_product_image_params =
+          Map.put(product_image_params, "image_url", data.secure_url)
+
+        File.rm(full_url)
+
+        update_product_image(socket, new_product_image_params)
+    end
+  end
+
+  defp update_product_image(socket, params) do
+    case ProductImages.update_product_image(
+           socket.assigns.product_image,
+           params
+         ) do
+      {:ok, _product_image} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Product image updated successfully")
+         |> push_redirect(to: socket.assigns.return_to)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_product_image(socket, :add_product_image, product_image_params) do
     uploaded_files =
       consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
         dest = Path.join([:code.priv_dir(:ngepa), "static", "uploads", Path.basename(path)])
@@ -47,24 +102,7 @@ defmodule NgepaWeb.ProductImageLive.FormComponent do
 
     File.rm(full_url)
 
-    save_product_image(socket, socket.assigns.action, new_product_image_params)
-  end
-
-  defp save_product_image(socket, :edit_product_image, product_image_params) do
-    case ProductImages.update_product_image(socket.assigns.product_image, product_image_params) do
-      {:ok, _product_image} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Product image updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
-
-  defp save_product_image(socket, :add_product_image, product_image_params) do
-    case ProductImages.create_product_image(product_image_params) do
+    case IO.inspect(ProductImages.create_product_image(new_product_image_params)) do
       {:ok, _product_image} ->
         {:noreply,
          socket
--- lib/ngepa_web/live/product_live/form_component.ex
+++ lib/ngepa_web/live/product_live/form_component.ex
@@ -10,6 +10,8 @@ defmodule NgepaWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
+     |> assign(:uploaded_files, [])
+     |> allow_upload(:image, accept: ~w(.jpg .png .jpeg), max_entries: 1)
      |> assign(:changeset, changeset)}
   end
 
@@ -28,7 +30,41 @@ defmodule NgepaWeb.ProductLive.FormComponent do
   end
 
   defp save_product(socket, :edit, product_params) do
-    case Products.update_product(socket.assigns.product, product_params) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+        dest = Path.join([:code.priv_dir(:ngepa), "static", "uploads", Path.basename(path)])
+
+        # The `static/uploads` directory must exist for `File.cp!/2`
+        # and MyAppWeb.static_paths/0 should contain uploads to work,.
+        File.cp!(path, dest)
+        {:ok, "/uploads/" <> Path.basename(dest)}
+      end)
+
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+
+    case List.first(uploaded_files) do
+      nil ->
+        new_product_params =
+          Map.put(product_params, "primary_image", socket.assigns.product.primary_image)
+
+        update_product(socket, new_product_params)
+
+      _ ->
+        full_url = "priv/static" <> List.first(uploaded_files)
+
+        {:ok, data} =
+          Cloudex.upload(full_url)
+
+        new_product_params =
+          Map.put(product_params, "primary_image", data.secure_url)
+
+        File.rm(full_url)
+        update_product(socket, new_product_params)
+    end
+  end
+
+  defp update_product(socket, params) do
+    case Products.update_product(socket.assigns.product, params) do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -41,7 +77,28 @@ defmodule NgepaWeb.ProductLive.FormComponent do
   end
 
   defp save_product(socket, :new, product_params) do
-    case Products.create_product(product_params) do
+    uploaded_files =
+      consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
+        dest = Path.join([:code.priv_dir(:ngepa), "static", "uploads", Path.basename(path)])
+
+        # The `static/uploads` directory must exist for `File.cp!/2`
+        # and MyAppWeb.static_paths/0 should contain uploads to work,.
+        File.cp!(path, dest)
+        {:ok, "/uploads/" <> Path.basename(dest)}
+      end)
+
+    {:noreply, update(socket, :uploaded_files, &(&1 ++ uploaded_files))}
+    full_url = "priv/static" <> List.first(uploaded_files)
+
+    {:ok, data} =
+      Cloudex.upload(full_url)
+
+    new_product_params =
+      Map.put(product_params, "primary_image", data.secure_url)
+
+    File.rm(full_url)
+
+    case Products.create_product(new_product_params) do
       {:ok, _product} ->
         {:noreply,
          socket
