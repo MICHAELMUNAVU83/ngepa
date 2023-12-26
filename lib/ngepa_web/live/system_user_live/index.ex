f1e1f03692fa9c382c3ec076078571e6593d75e3
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-12-26 11:53:23 +0300

    Added delivery agent

--- lib/ngepa_web/live/system_user_live/index.ex
+++ lib/ngepa_web/live/system_user_live/index.ex
@@ -7,10 +7,15 @@ defmodule NgepaWeb.SystemUserLive.Index do
 
   @impl true
   def mount(_params, session, socket) do
-    users = Accounts.list_users()
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+
+    users =
+      Accounts.list_users()
+      |> Enum.filter(fn user -> user.id != current_user.id end)
 
     {:ok,
      socket
+     |> assign(:current_user, current_user)
      |> assign(:users, users)}
   end
 
@@ -22,5 +27,47 @@ defmodule NgepaWeb.SystemUserLive.Index do
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing System Users")
+  end
+
+  def handle_event("make_admin", %{"id" => id}, socket) do
+    user = Accounts.get_user!(id)
+    {:ok, _user} = Accounts.update_user(user, %{role: "admin"})
+
+    {:noreply,
+     socket
+     |> assign(
+       :users,
+       Accounts.list_users()
+       |> Enum.filter(fn user -> user.id != socket.assigns.current_user.id end)
+     )
+     |> put_flash(:info, "User promoted to admin")}
+  end
+
+  def handle_event("make_user", %{"id" => id}, socket) do
+    user = Accounts.get_user!(id)
+    {:ok, _user} = Accounts.update_user(user, %{role: "user"})
+
+    {:noreply,
+     socket
+     |> assign(
+       :users,
+       Accounts.list_users()
+       |> Enum.filter(fn user -> user.id != socket.assigns.current_user.id end)
+     )
+     |> put_flash(:info, "User made a normal user")}
+  end
+
+  def handle_event("make_delivery_agent", %{"id" => id}, socket) do
+    user = Accounts.get_user!(id)
+    {:ok, _user} = Accounts.update_user(user, %{role: "delivery_agent"})
+
+    {:noreply,
+     socket
+     |> assign(
+       :users,
+       Accounts.list_users()
+       |> Enum.filter(fn user -> user.id != socket.assigns.current_user.id end)
+     )
+     |> put_flash(:info, "User given delivery fulfilment rights")}
   end
 end
